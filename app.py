4547180fb95ae2f24c5d7bf9686382c7491d481d
sreepathi-ravikumar <rksreepathi1@gmail.com>
2025-07-06 14:42:53 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,16 +5,37 @@ import os
 app = Flask(__name__)
 CORS(app)  # Enable CORS for frontend-backend connection
 
+prompt=None
+
 @app.route('/api/submit', methods=['POST'])
 def receive_input():
+    global prompt
     data = request.get_json()
     user_input = data.get('input', '')
-    print(f"Received input: {user_input}")
+    prompt=user_input
 
-    return jsonify({"message": f"Backend received: {user_input}"})
+@app.route('/api/get-string', methods=['GET'])
+def get_string():
+    global prompt
+    return jsonify({"data": prompt})
+
+@app.route('/api/receive', methods=['POST'])
+def receive():
+    data = request.get_json()
+    final_string = data.get("final")
+    print("Received from frontend:", final_string)
+    return jsonify({"message": f"Python received final string: {final_string}"})
 
 
 if __name__ == '__main__':
     # For local testing, use port 5000
     port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
+
+
+
+
+
+
+
+
